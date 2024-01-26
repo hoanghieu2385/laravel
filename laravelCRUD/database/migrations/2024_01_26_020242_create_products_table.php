017153ba1bc592b73add99933296ee5bbc821004
hoanghieu2385 <hoangminhhieu230805@gmail.com>
2024-01-26 09:32:23 +0700

    create Template sẵn dùng

--- laravelCRUD/database/migrations/2024_01_26_020242_create_products_table.php
+++ laravelCRUD/database/migrations/2024_01_26_020242_create_products_table.php
@@ -1,27 +1,61 @@
 <?php
 
+
+
 use Illuminate\Database\Migrations\Migration;
+
 use Illuminate\Database\Schema\Blueprint;
+
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+
+
+class CreateProductsTable extends Migration
+
 {
+
     /**
+
      * Run the migrations.
+
+     *
+
+     * @return void
+
      */
-    public function up(): void
+
+    public function up()
+
     {
+
         Schema::create('products', function (Blueprint $table) {
+
             $table->id();
+
+            $table->string('name');
+
+            $table->text('detail');
+
             $table->timestamps();
         });
     }
 
+
+
     /**
+
      * Reverse the migrations.
+
+     *
+
+     * @return void
+
      */
-    public function down(): void
+
+    public function down()
+
     {
+
         Schema::dropIfExists('products');
     }
-};
+}
