4ef078146d014636466dc5865851f7f3e4b89c66
hoanghieu2385 <hoangminhhieu230805@gmail.com>
2024-01-29 09:26:39 +0700

    example

--- example/routes/web.php
+++ example/routes/web.php
@@ -1,10 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ContactController;
-
-  
-
 
 /*
 |--------------------------------------------------------------------------
@@ -20,6 +16,3 @@ use App\Http\Controllers\ContactController;
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-Route::resource('contact', ContactController::class);
